7b08e9c3727df09593ce6be56f5ee1dab1c8f7e6
ZoidsRobot <113953968+ZoidsRobot@users.noreply.github.com>
2023-03-13 17:40:59 +0800

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -9,12 +9,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = 3599592
-API_HASH = "80865dfca1e192f81931cbf61203cfe7"
-BOT_TOKEN = "5851808236:AAEc8zGr6Rd1EKbN5x3kAXXICMQepT24mJY"
-FORCESUB = 1485129059
-ACCESS = int("-1001856033450")
-MONGODB_URI = "mongodb+srv://Caanolisurukar:Caanolisurukar@cluster0.gs9clvq.mongodb.net/?retryWrites=true&w=majority"
-AUTH_USERS = 1058482162
+API_ID = 9774346
+API_HASH = "a92aed7d74654a563af4b07efbcd88e9"
+BOT_TOKEN = "6278815762:AAF6Vkg0hki1ZxxOmAzWbeCsZRPyTXI0Kfc"
+FORCESUB = 1809733770
+ACCESS = int("-1001809733770")
+MONGODB_URI = "mongodb+srv://Zoids:Rextor99 @manage.pyt3xq1.mongodb.net/?retryWrites=true&w=majority"
+AUTH_USERS = 907544310
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
