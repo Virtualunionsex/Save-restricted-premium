665e1e9bb4143070aaf62af5e1fbd1bf0f76156f
ZoidsRobot <113953968+ZoidsRobot@users.noreply.github.com>
2023-03-13 18:18:56 +0800

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,8 +12,8 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 API_ID = 9774346
 API_HASH = "a92aed7d74654a563af4b07efbcd88e9"
 BOT_TOKEN = "6278815762:AAF6Vkg0hki1ZxxOmAzWbeCsZRPyTXI0Kfc"
-FORCESUB = 1809733770
-ACCESS = int("-1001809733770")
+FORCESUB = 809733770
+ACCESS = int("-1001509774429")
 MONGODB_URI = "mongodb+srv://RyanMusic:Rextor99@cluster0.pxtgxfl.mongodb.net/?retryWrites=true&w=majority"
 AUTH_USERS = 907544310
 
