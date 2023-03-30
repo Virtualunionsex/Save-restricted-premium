56ef90a529323f155173629d56d8e47a2cc2fb3e
MalikGit <97935254+Virtualunionsex@users.noreply.github.com>
2023-03-31 06:49:41 +0700

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -14,7 +14,7 @@ API_HASH = "29b3ffa9245c07f05375b92f38e8f13d"
 BOT_TOKEN = "5716827359:AAE3NJPyZkXHATL8m3AmLVxFy0-NNhKOOog"
 FORCESUB = -1001528080636
 ACCESS = int("-1001743550303")
-MONGODB_URI = "mongodb+srv://Caanolisurukar:Caanolisurukar@cluster0.gs9clvq.mongodb.net/?retryWrites=true&w=majority"
+MONGODB_URI = "mongodb+srv://Copet0:Malik10_@cluster0.0grdtud.mongodb.net/?retryWrites=true&w=majority"
 AUTH_USERS = 1715348447
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
