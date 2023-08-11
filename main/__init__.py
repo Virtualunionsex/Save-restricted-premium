98b01a044a0a2907a5a9c597d844f2fa58002741
Azure12321 <140412268+Azure12321@users.noreply.github.com>
2023-08-11 16:46:52 +0700

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -14,7 +14,7 @@ API_HASH = "40dc918d6f9bdd391f2fbfb5bbd0328c"
 BOT_TOKEN = "6382292112:AAHZDf9xgTUEpllpkclidKn_toO02hvYr58"
 FORCESUB = -1001743550303
 ACCESS = int("-1001743550303")
-MONGODB_URI = "mongodb+srv://RyanMusic:Rextor99@cluster0.pxtgxfl.mongodb.net/?retryWrites=true&w=majority"
+MONGODB_URI = "mongodb+srv://projeklisatiga:Malik10_@cluster0.tgthj.mongodb.net/?retryWrites=true&w=majority"
 AUTH_USERS = 6452204836 5657257558
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
